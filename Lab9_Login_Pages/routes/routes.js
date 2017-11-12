d6e4d969a82a79c3031b0ad9cbe92c53b78ed1a3
sourabh kulkarni <skulkar6@stevens.edu>
2017-11-11 22:37:53 -0500

    get / route

--- Lab9_Login_Pages/routes/routes.js
+++ Lab9_Login_Pages/routes/routes.js
@@ -3,12 +3,27 @@ let router = express.Router();
 const passport = require('passport');
 
 
-router.get("/",function(req,res){
-   res.send("HII");
-});
+// router.get("/",function(req,res){
+//    res.send("HII");
+// });
+
+let configureRoutes = (app) => {
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+app.get('/', (req, res) => {
+    if(req.isAuthenticated()){
+       res.redirect('/private')
+    }else{
+        res.redirect('/login')
+    }
+})
+
+
+}
 
 
 
 
 
-module.exports = router;
+module.exports = configureRoutes;
